19a751aceb97cfdec019fc66b032c852e77f6c6b
Richwell Antwi <145505989+Richwell111@users.noreply.github.com>
2025-09-13 23:28:34 +0000

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,47 +1,17 @@
-// Director interface
-export interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+// String literal type Subjects
+type Subjects = "Math" | "History";
+
+// Function teachClass
+function teachClass(todayClass:Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  if (todayClass === "History") {
+    return "Teaching History";
+  }
+  return "";
 }
 
-// Teacher interface
-export interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-// Director class
-export class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
-}
-
-// Teacher class
-export class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
-}
-
-// createEmployee function
-export function createEmployee(salary: number | string): Director | Teacher {
-  if (salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
-}
+// Test logs
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
