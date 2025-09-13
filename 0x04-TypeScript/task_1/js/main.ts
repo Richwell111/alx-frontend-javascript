452417c9a0cff096d1af9dc5acf539d675f70f4a
Richwell111 <bcict20103@gmail.com>
2025-09-13 23:01:53 +0000

    added other tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,71 +1,31 @@
-// 1. Teacher Interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
+// Teacher interface
+export interface Teacher {
+  firstName: string;
+  lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // allows extra attributes like "contract"
+  [key: string]: any; // allows additional attributes
 }
 
-const teacher3: Teacher = {
+// Director interface extending Teacher
+export interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage
+const teacher: Teacher = {
   firstName: "John",
-  fullTimeEmployee: false,
   lastName: "Doe",
+  fullTimeEmployee: true,
   location: "London",
   contract: false,
 };
 
-console.log(teacher3);
-
-// 2. Directors Interface extending Teacher
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-const director1: Directors = {
-  firstName: "John",
-  lastName: "Doe",
-  location: "London",
+const director: Director = {
+  firstName: "Jane",
+  lastName: "Smith",
   fullTimeEmployee: true,
+  location: "New York",
   numberOfReports: 17,
 };
-
-console.log(director1);
-
-// 3. printTeacher function & interface
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-console.log(printTeacher("John", "Doe")); // J. Doe
-
-// 4. StudentClass
-interface StudentConstructor {
-  new (firstName: string, lastName: string): StudentClassInterface;
-}
-
-interface StudentClassInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-class StudentClass implements StudentClassInterface {
-  constructor(public firstName: string, public lastName: string) {}
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
-
-const student = new StudentClass("Richwell", "Antwi");
-console.log(student.displayName()); // Richwell
-console.log(student.workOnHomework()); // Currently working
